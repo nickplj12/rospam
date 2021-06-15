ec10947eab3f5de13d9aff9dd58a00394fa2811e
nickpythonluajava <78268270+nickpythonluajava@users.noreply.github.com>
2021-06-15 11:04:32 -0400

    Update rospam.py

--- rospam.py
+++ rospam.py
@@ -3,10 +3,12 @@ import time
 import pydirectinput
 
 prompt = pyautogui.prompt('text to be spammed')
+delay = 1 #the delay feature. this is how many seconds until the next chat should be sent.
+
 
 while(True):
 	pyautogui.press('/')
 	pyautogui.write(prompt)
 	pydirectinput.press('enter')
 	print('Printed')
-	time.sleep(1)
+	time.sleep(delay)
